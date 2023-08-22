8040654149ee8daedd4314ed6edb68e6918fca5c
smithjadhav <mr.smiteshjadhav@gmail.com>
2023-08-22 22:07:25 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+from   os import environ
+
+class Config(object):
+
+    DEBUG = False
+    TESTING = False
+    
+    basedir    = os.path.abspath(os.path.dirname(__file__))
+
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False
+
+ 
+class DebugConfig(Config):
+    DEBUG = False
